ca9d59c2dddeb777c6bd7a9d6be772d6cfef8003
Jonathon Ross <jonathon.ross@synapse.com>
2018-09-20 19:58:08 -0700

    label images or directories. Scale image size to 75% of the screen. Print error message as full sized image.

--- scripts/user_label_image.py
+++ scripts/user_label_image.py
@@ -15,18 +15,33 @@ import os
 import argparse
 import cv2
 import numpy as np
+import shutil
+from win32api import GetSystemMetrics
 
 LEFT_KEY = 2424832
 RIGHT_KEY = 2555904
+ESCAPE_KEY = 27
+BACKSPACE_KEY = 8
 RED = ( 255, 0, 0 )
 GREEN = ( 0, 255, 0 )
 WHITE = ( 255, 255, 255 )
+INVALID_STRING = "not_cat"
+VALID_STRING = "cat"
 
 IMAGE_TEXT = "Does Image Contain Cat?"
 IMAGE_PATH = os.path.join( os.path.dirname( os.path.realpath(__file__) ), "images" )
 USAGE_IMG = os.path.join( IMAGE_PATH, "usage.jpg" )
 ARROWS_IMG = os.path.join( IMAGE_PATH, "cat-notcat.jpg" )
 
+
+def get_max_window_size():
+
+  width = GetSystemMetrics(0)
+  height = GetSystemMetrics(1)
+
+  max_size = min(width, height) * 0.75
+
+  return int(max_size)
 
 def create_blank( image, rgb_color=(0, 0, 0)):
 
@@ -46,6 +61,17 @@ def create_blank( image, rgb_color=(0, 0, 0)):
 
 
 def display_image_wait_key( image, delay_ms=0 ):
+
+  max_window_size = get_max_window_size()
+  window_size = max( image.shape[0], image.shape[1] )
+
+  ratio = max_window_size / window_size
+
+  dimensions = ( int( image.shape[1] * ratio ) , int( image.shape[0] * ratio) )
+  
+  # resize image
+  image = cv2.resize(image, dimensions)
+
   cv2.imshow(IMAGE_TEXT, image)
   return cv2.waitKeyEx( delay_ms )
 
@@ -63,93 +89,91 @@ def concatenate_images( image1, image2 ):
   return np.concatenate( ( image1, resized ), axis=0)
 
 
-def sort_images( image_dir, image_classifications, outdir ):
+def move_images( image_dir, image_labels, outdir ):
   
-  cat_dir = os.path.join( outdir, "cats" )
-  not_cat_dir = os.path.join( outdir, "not_cats" )
-
-  if not os.path.isdir( cat_dir ):
-    os.mkdir( cat_dir )  
-  
-  if not os.path.isdir( not_cat_dir ):
-    os.mkdir( not_cat_dir )  
+  valid_dir = os.path.join( outdir, VALID_STRING )
+  invalid_dir = os.path.join( outdir, INVALID_STRING )
 
   # iterate over all the labeled images and put them within a foler
-  for file in image_classifications:
+  for file in image_labels:
 
     file_dir = file.split(image_dir)[1] # split path to find any sub directories
     if file_dir[0] == '\\':
       file_dir = file_dir[1:] # get rid of the first slash on the filename
 
-    if image_classifications[file] == "cat":
-      new_file = os.path.join( cat_dir, file_dir )
+    if image_labels[file] == VALID_STRING:
+      new_file = os.path.join( valid_dir, file_dir )
 
-    if image_classifications[file] == "not_cat":
-      new_file = os.path.join( not_cat_dir, file_dir )
+    if image_labels[file] == INVALID_STRING:
+      new_file = os.path.join( invalid_dir, file_dir )
 
     # make sure the new sub directory exists
     new_file_dir = os.path.dirname(new_file)
     if not os.path.isdir( new_file_dir ):
-      os.mkdir( new_file_dir )
+      os.makedirs( new_file_dir )
 
-    print( file, new_file )
     os.rename(file, new_file )
 
 
-def get_image_labels( files ):
+def move_directories( image_dir, directory_labels, outdir ):
+  
+  valid_dir = os.path.join( outdir, VALID_STRING )
+  invalid_dir = os.path.join( outdir, INVALID_STRING )
 
-  num_files = len( files )
-  index = 0
+  # iterate over all the labeled images and put them within a foler
+  for directory in directory_labels:
 
-  image_classifications = dict()
+    file_dir = directory.split(image_dir)[1] # split path to find any sub directories
+    if file_dir[0] == '\\':
+      file_dir = file_dir[1:] # get rid of the first slash on the filename
+
+    if directory_labels[directory] == VALID_STRING:
+      new_dir = os.path.join( valid_dir, file_dir )
+
+    if directory_labels[directory] == INVALID_STRING:
+      new_dir = os.path.join( invalid_dir, file_dir )
+
+    # make sure the new sub directory exists
+    new_file_dir = os.path.dirname(new_dir)
+    if not os.path.isdir( new_file_dir ):
+      os.makedirs( new_file_dir )
+
+    shutil.move(directory, new_dir )
+
+
+def disp_image_get_input( file ):
 
   # iterate over all files and add label
-  while index < num_files:
-    file_name = files[index]
+  while True:
     
     # display image to be labeled
-    image = cv2.imread( file_name, cv2.IMREAD_COLOR)
+    image = cv2.imread( file, cv2.IMREAD_COLOR)
     image = concatenate_images( image, ARROWS_IMG )
     key = display_image_wait_key( image, 0 )
 
     # wait for user input
-    done = False
-    while not done:
-      if key == LEFT_KEY:
-        update = cv2.add(image, create_blank(image, RED))
-        display_image_wait_key( update, 100)
-        image_classifications[file_name] = "not_cat"
-        done = True
-        index = index + 1
+    if key == LEFT_KEY:
+      update = cv2.add(image, create_blank( image, RED) )
+      display_image_wait_key( update, 100)
+      return key
 
-      elif key == RIGHT_KEY:
-        update = cv2.add(image, create_blank(image, GREEN))
-        display_image_wait_key( update, 100)
-        image_classifications[file_name] = "cat"
-        done = True
-        index = index + 1
+    elif key == RIGHT_KEY:
+      update = cv2.add( image, create_blank( image, GREEN ) )
+      display_image_wait_key( update, 100 )
+      return key
 
-      elif key == 27: # escape
-        # quit
-        cv2.destroyAllWindows()
-        return image_classifications
+    elif key == ESCAPE_KEY:
+      return key
 
-      # TODO: add user instructions that backspace can be used to back up
-      elif key == 8: # backspace
-        done = True
+    elif key == BACKSPACE_KEY:
+      return key
 
-        # ensure we don't go negative with the index
-        if ( index > 0 ):
-          index = index - 1
+    else:
+      image = cv2.imread( USAGE_IMG, cv2.IMREAD_COLOR)
+      key = display_image_wait_key( image, 0)
+      #todo, change image text to contain press any key to continue
 
-      else:
-        update = concatenate_images( image, USAGE_IMG )
-        key = display_image_wait_key( update, 0)
-
-  return image_classifications
-
-
-def get_directory_label( files ):
+def display_directory_get_input( files ):
 
   num_files = len( files )
 
@@ -167,25 +191,23 @@ def get_directory_label( files ):
       if key == LEFT_KEY:
         update = cv2.add(image, create_blank(image, RED))
         display_image_wait_key( update, 100)
-        return "not_cat"
+        return key
 
       elif key == RIGHT_KEY:
         update = cv2.add(image, create_blank(image, GREEN))
         display_image_wait_key( update, 100)
-        return "cat"
-      
-      elif key == 27: # escape
-        cv2.destroyAllWindows()
-        return "quit"
+        return key
 
-      elif key == 8: # backspace
-        return "back"
+      elif key == ESCAPE_KEY:
+        return key
+
+      elif key == BACKSPACE_KEY:
+        return key
 
       elif key != -1:
         image = cv2.imread( USAGE_IMG, cv2.IMREAD_COLOR)
         key = display_image_wait_key( image, 0)
         #todo, change image text to contain press any key to continue
-
 
 def list_all_jpgs( directory ):
   jpeg_files = []
@@ -200,20 +222,87 @@ def list_all_jpgs( directory ):
 
 def user_label_images( image_dir, outdir, parse_burst ):
   
-  # TODO, this may make more sense to iterate over all the files and label each image instead of calling get_image_labels()
+  # TODO: there's a lot of duplicated code here, maybe function pointers?
+  ######################### sort individual files #########################
   if not parse_burst:
     images_to_label = list_all_jpgs( image_dir )
-    image_classificationss = get_image_labels( images_to_label )
-    sort_images( image_dir, image_classificationss, outdir )
+    if len(images_to_label) == 0:
+      return
 
+    image_labels = dict()
+    done = False
+    index = 0
+    while not done: 
+      image = images_to_label[index]
+      key = disp_image_get_input( image )
+      
+      if key == LEFT_KEY:
+        image_labels[image] = INVALID_STRING
+        index = index + 1
+
+      elif key == RIGHT_KEY:
+        image_labels[image] = VALID_STRING
+        index = index + 1
+
+      elif key == BACKSPACE_KEY:
+        # ensure we don't go negative with the index
+        if ( index > 0 ):
+          index = index - 1
+
+        # remove the entry from dict
+        image = images_to_label[index]
+        del image_labels[image]
+
+      elif key == ESCAPE_KEY:
+        cv2.destroyAllWindows()
+        done = True
+      
+      if index >= len(images_to_label):
+        done = True
+
+    move_images( image_dir, image_labels, outdir )
+
+  ######################### sort image bursts #########################
   else:
     directories = next(os.walk(image_dir))[1]
-    for directory in directories:
-      search_dir = os.path.join(image_dir, directory)
-      jpegs_in_dir = list_all_jpgs( search_dir )
-      label = get_directory_label( jpegs_in_dir )
-      print( label)
-      #TODO move whole directory based on label
+    if len(directories) == 0:
+      return
+
+    directory_labels = dict()
+    index = 0
+    done = False
+
+    while not done:
+
+      burst_dir = os.path.join( image_dir, directories[index] )
+      jpegs_in_dir = list_all_jpgs( burst_dir )
+      key = display_directory_get_input( jpegs_in_dir )
+      
+      if key == LEFT_KEY:
+        directory_labels[burst_dir] = INVALID_STRING
+        index = index + 1
+
+      elif key == RIGHT_KEY:
+        directory_labels[burst_dir] = VALID_STRING
+        index = index + 1
+
+      elif key == BACKSPACE_KEY:
+        # ensure we don't go negative with the index
+        if ( index > 0 ):
+          index = index - 1
+
+        # remove the entry from dict
+        burst_dir = os.path.join( image_dir, directories[index] )
+        del directory_labels[burst_dir]
+
+      elif key == ESCAPE_KEY:
+        cv2.destroyAllWindows()
+        done = True
+      
+      if index >= len(directories):
+        done = True
+
+    move_directories( image_dir, directory_labels, outdir )
   
 def main():
   parser = argparse.ArgumentParser()
